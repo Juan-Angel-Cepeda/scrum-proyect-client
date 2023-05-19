4e2d64ffbbad61403d8c67fa390eff2882428441
angelcepeda <jangelcepeda@protonmail.com>
2023-05-19 11:02:13 -0700

    unos componentes que no hacen nada

---
+++ src/components/ColumnsList.vue
@@ -0,0 +1,31 @@
+<template>
+    <h3>Columnas</h3>
+    <router-link to="/columns">Columnas</router-link> 
+
+
+</template>
+
+<script>
+    import axios from 'axios'
+    export default{
+        name:'ColumnsList',
+        data(){
+            return{
+                columns:null
+            }
+        },
+        methods:{
+            list(){
+                axios.get('')
+                     .then(res => this.directors = res.data.obj);
+            }
+        },
+        mounted(){
+            this.list()
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/indexComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>
+        Welcome to Scrum Proyect
+    </h1>
+</template>
+<script></script>
+<style></style>
